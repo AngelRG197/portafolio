a03a5d7842f1878afdcbb79e429651e803a33c46
ZyberPro <zyberprooficial@gmail.com>
2026-07-01 11:26:16 -0400

    Configurar GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Curriculum_duo',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
   },
 }
 
